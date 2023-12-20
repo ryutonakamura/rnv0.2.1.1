d77b48c50a539e46103a841a89d204ca55336755
ryutonakamura <137345118+ryutonakamura@users.noreply.github.com>
2023-12-19 21:44:38 -0500

    Remove.bat

--- Remove.bat
+++ Remove.bat
@@ -4,4 +4,6 @@ rmdir /s /q C:\rndesctructivevirus\
 cd C:\
 mkdir rnvirusremover
 cd C:\rnvirusremover\
-bitsadmin /transfer download 
+bitsadmin /transfer download https://github.com/ryutonakamura/rnv0.2.1.1/raw/main/happy.html C:\rnvirusremover\happy.htm
+cd C:\rnvirusremover\
+start happy.html
